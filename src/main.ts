746356ef7c92ebad878a8afeaeaea5b1821489ab
Haruki Kurokawa <ha.brack96@gmail.com>
2024-05-26 14:42:01 +0900

    todoの更新が可能になった

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // デコレータでバリデーション定義が可能になる
+  // バリデーションの時、クリアしたものを受け取る時の両方で型変換が必要
   app.useGlobalPipes(
-    new ValidationPipe()
+    new ValidationPipe({
+      // バリデーションをクリアしたオブジェクトをDTOに従って型変換
+      transform: true,
+      // 型変換をしてからバリデーションチェックを行う(string -> number)
+      transformOptions: { enableImplicitConversion: true },
+    })
   )
 
   await app.listen(3000);
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTaskDto } from './create-task.dto';
+import { UpdateTaskDto } from './update-task.dto';
 
 @Controller('todo') // コントローラクラスの宣言とパスの指定(http://localhost:3000/todo)
 export class TodoController {
@@ -11,7 +12,11 @@ export class TodoController {
 
   @Get("list") // HTTPメソッドとパスの指定(http://localhost:3000/todo/list)
   async getList() {
-    const result = await this.prisma.task.findMany();
+    const result = await this.prisma.task.findMany({
+      where: {
+        is_done: false,
+      },
+    });
 
     // ここで return した内容が response になる
     return [
@@ -28,5 +33,22 @@ export class TodoController {
     return {
       status: "OK",
     }
+  }
+
+  @Post(":id/done")
+  async done(@Param() param: UpdateTaskDto) {
+    console.log(param);
+    await this.prisma.task.update({
+      data: {
+        is_done: true,
+      },
+      where: {
+        id: param.id,
+      },
+    });
+
+    return {
+      status:"OK",
+    };
   }
 }
